a8635cc6269ec8236a6bb1123219d8bf00ce52a1
David Turner <david@freetype.org>
2000-02-10 16:15:11 +0000

    Very simple AFM Parser

---
+++ src/type1/t1afm.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *
+ * t1afm.h  - support for reading Type 1 AFM files
+ *
+ *
+ ***************************************************************************/
+
+#ifndef T1AFM_H
+#define T1AFM_H
+
+#include <ftobjs.h>
+
+/* In this version, we only read the kerning table from the */
+/* AFM file. We may add support for ligatures a bit later.. */
+
+typedef struct T1_Kern_Pair_
+{
+  FT_UInt   glyph1;
+  FT_UInt   glyph2;
+  FT_Vector kerning;
+  
+} T1_Kern_Pair;
+
+
+typedef struct T1_AFM_
+{
+  FT_Int        num_pairs;
+  T1_Kern_Pair* kern_pairs;
+
+} T1_AFM;
+
+
+LOCAL_DEF
+FT_Error  T1_Read_AFM( FT_Stream stream,
+                       FT_Face   face );
+
+LOCAL_DEF
+void      T1_Done_AFM( FT_Memory  memory,
+                       T1_AFM*    afm );
+
+LOCAL_DEF
+void  T1_Get_Kerning( T1_AFM*     afm,
+                      FT_UInt     glyph1,
+                      FT_UInt     glyph2,
+                      FT_Vector*  kerning );
+
+#endif /* T1AFM_H */
