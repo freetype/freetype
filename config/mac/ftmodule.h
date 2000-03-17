05ef28c4ada3193748f381285461e83569a64fab
Just van Rossum <just@letterror.com>
2000-03-17 11:48:25 +0000

    Added mac FOND driver, and use t1z driver instead of the "old" t1 driver.

--- config/mac/ftmodule.h
+++ config/mac/ftmodule.h
@@ -1,4 +1,5 @@
+FT_DRIVER(fond_driver_interface)
 FT_DRIVER(psnames_driver_interface)
 FT_DRIVER(sfnt_driver_interface)
 FT_DRIVER(tt_driver_interface)
-FT_DRIVER(t1_driver_interface)
+FT_DRIVER(t1z_driver_interface)
