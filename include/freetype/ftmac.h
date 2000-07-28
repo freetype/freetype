e973e66a0f7108c719c2c632098170fbc90034c5
Just van Rossum <just@letterror.com>
2000-07-28 01:08:32 +0000

    Header for one Mac-specific additional API call.

---
+++ include/freetype/ftmac.h
@@ -0,0 +1,64 @@
+/***************************************************************************/
+/*                                                                         */
+/*  ftmac.h                                                                */
+/*                                                                         */
+/*    Additional Mac-specific API.                                         */
+/*                                                                         */
+/*  Copyright 1996-2000 by                                                 */
+/*  Just van Rossum, David Turner, Robert Wilhelm, and Werner Lemberg.     */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+/* NOTE: include this file after <freetype/freetype.h> and after the
+   Mac-specific <Types.h> header (or any other Mac header that includes
+   <Types.h>); we use Handle type. */
+
+
+#ifndef FT_MAC_H
+#define FT_MAC_H
+
+
+  /*************************************************************************/
+  /*                                                                       */
+  /* <Function>                                                            */
+  /*    FT_New_Face_From_FOND                                              */
+  /*                                                                       */
+  /* <Description>                                                         */
+  /*    Creates a new face object from an FOND resource.                   */
+  /*                                                                       */
+  /* <InOut>                                                               */
+  /*    library    :: A handle to the library resource.                    */
+  /*                                                                       */
+  /* <Input>                                                               */
+  /*    fond       :: An FOND resource.                                    */
+  /*                                                                       */
+  /*    face_index :: only supported for the -1 `sanity check'             */
+  /*                  special case.                                        */
+  /*                                                                       */
+  /* <Output>                                                              */
+  /*    aface      :: A handle to a new face object.                       */
+  /*                                                                       */
+  /* <Return>                                                              */
+  /*    FreeType error code.  0 means success.                             */
+  /*                                                                       */
+  /* <Notes>                                                               */
+  /*    This function can be used to create FT_Face abjects from fonts     */
+  /*    that are installed in the system like so:                          */
+  /*      fond = GetResource( 'FOND', fontName );                          */
+  /*      error = FT_New_Face_From_FOND( library, fond, 0, &face );        */
+  /*                                                                       */
+  FT_EXPORT_FUNC( FT_Error )  FT_New_Face_From_FOND(
+                                FT_Library  library,
+                                Handle      fond,
+                                FT_Long     face_index,
+                                FT_Face*    aface );
+
+
+#endif /* FT_MAC_H */
