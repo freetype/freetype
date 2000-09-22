f50c2954d4706af4762f7f40c6e933bcef507528
Werner Lemberg <wl@gnu.org>
2000-09-22 06:52:20 +0000

    Fixed typo in ftimage.h
    Fixed sbit on/off flag in cache code.

--- include/freetype/ftimage.h
+++ include/freetype/ftimage.h
@@ -245,7 +245,7 @@
   /*    tags       :: A pointer to an array of `n_points' chars, giving    */
   /*                  giving each outline point's type.  If bit 0 is       */
   /*                  unset, the point is 'off' the curve, i.e. a Bezier   */
-  /*                  control point, while it is `on' when unset.          */
+  /*                  control point, while it is `on' when set.            */
   /*                                                                       */
   /*                  Bit 1 is meaningful for `off' points only.  If set,  */
   /*                  it indicates a third-order Bezier arc control point; */
