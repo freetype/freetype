a4e2894e03de65c5e0cd14b11b6fa30b14ed6769
David Turner <david@freetype.org>
2000-10-26 00:30:33 +0000

    simple renaming of directories:
      "type1z"  -> "type1"
      "raster1" -> "raster"
    
    note that I didn't rename all files. We'll have to endure
    the "z1..." crazyness unless we perform a _big_ sed on
    the sources :-)

--- src/raster/module.mk
+++ src/raster/module.mk
@@ -13,10 +13,10 @@
 # fully.
 
 
-make_module_list: add_raster1_module
+make_module_list: add_raster_module
 
-add_raster1_module:
+add_raster_module:
 	$(OPEN_DRIVER)ft_raster1_renderer_class$(CLOSE_DRIVER)
-	$(ECHO_DRIVER)raster1   $(ECHO_DRIVER_DESC)monochrome bitmap renderer$(ECHO_DRIVER_DONE)
+	$(ECHO_DRIVER)raster    $(ECHO_DRIVER_DESC)monochrome bitmap renderer$(ECHO_DRIVER_DONE)
 
 # EOF
---
+++ src/type1/rules.mk
@@ -0,0 +1,72 @@
+#
+# FreeType 2 Type1z driver configuration rules
+#
+
+
+# Copyright 1996-2000 by
+# David Turner, Robert Wilhelm, and Werner Lemberg.
+#
+# This file is part of the FreeType project, and may only be used, modified,
+# and distributed under the terms of the FreeType project license,
+# LICENSE.TXT.  By continuing to use, modify, or distribute this file you
+# indicate that you have read the license and understand and accept it
+# fully.
+
+
+# Type1 driver directory
+#
+T1_DIR  := $(SRC_)type1
+T1_DIR_ := $(T1_DIR)$(SEP)
+
+
+# compilation flags for the driver
+#
+T1_COMPILE := $(FT_COMPILE)
+
+
+# Type1 driver sources (i.e., C files)
+#
+T1_DRV_SRC := $(T1_DIR_)z1parse.c  \
+              $(T1_DIR_)z1load.c   \
+              $(T1_DIR_)z1driver.c \
+              $(T1_DIR_)z1afm.c    \
+              $(T1_DIR_)z1gload.c  \
+              $(T1_DIR_)z1objs.c
+
+# Type1 driver headers
+#
+T1_DRV_H := $(T1_DRV_SRC:%.c=%.h) \
+             $(T1_DIR_)z1tokens.h
+
+
+# Type1z driver object(s)
+#
+#   T1_DRV_OBJ_M is used during `multi' builds
+#   T1_DRV_OBJ_S is used during `single' builds
+#
+T1_DRV_OBJ_M := $(T1_DRV_SRC:$(T1_DIR_)%.c=$(OBJ_)%.$O)
+T1_DRV_OBJ_S := $(OBJ_)type1.$O
+
+# Type1z driver source file for single build
+#
+T1_DRV_SRC_S := $(T1_DIR_)type1.c
+
+
+# Type1z driver - single object
+#
+$(T1_DRV_OBJ_S): $(T1_DRV_SRC_S) $(T1_DRV_SRC) $(FREETYPE_H) $(T1_DRV_H)
+	$(T1_COMPILE) $T$@ $(T1_DRV_SRC_S)
+
+
+# Type1z driver - multiple objects
+#
+$(OBJ_)%.$O: $(T1_DIR_)%.c $(FREETYPE_H) $(T1_DRV_H)
+	$(T1_COMPILE) $T$@ $<
+
+
+# update main driver object lists
+#
+DRV_OBJS_S += $(T1_DRV_OBJ_S)
+DRV_OBJS_M += $(T1_DRV_OBJ_M)
+
+# EOF
