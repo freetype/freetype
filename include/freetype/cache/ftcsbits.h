dd33561ecb0cb0cf7459e49d11d53a55199f3891
Werner Lemberg <wl@gnu.org>
2000-10-31 19:42:53 +0000

    Formatting.
    Adding a .cvsignore file.

--- include/freetype/cache/ftcsbits.h
+++ include/freetype/cache/ftcsbits.h
@@ -2,7 +2,7 @@
 /*                                                                         */
 /*  ftcsbits.h                                                             */
 /*                                                                         */
-/*    a small-bitmaps cache (specification).                               */
+/*    A small-bitmap cache (specification).                                */
 /*                                                                         */
 /*  Copyright 2000 by                                                      */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
@@ -15,25 +15,27 @@
 /*                                                                         */
 /***************************************************************************/
 
+
 #ifndef FTCSBITS_H
 #define FTCSBITS_H
 
+
 #include <freetype/cache/ftcchunk.h> 
+
 
 #ifdef __cplusplus
   extern "C" {
 #endif
 
- /* handle to small bitmap */
-  typedef struct FTC_SBitRec_*   FTC_SBit;
 
+  /* handle to small bitmap */
+  typedef struct FTC_SBitRec_*  FTC_SBit;
 
- /* handle to small bitmap cache */
-  typedef struct FTC_SBit_CacheRec_*   FTC_SBit_Cache;
+  /* handle to small bitmap cache */
+  typedef struct FTC_SBit_CacheRec_*  FTC_SBit_Cache;
 
-
- /* a compact structure used to hold a single small bitmap */  
-  typedef struct FTC_SBitRec_
+  /* a compact structure used to hold a single small bitmap */  
+  typedef struct  FTC_SBitRec_
   {
     FT_Byte   width;
     FT_Byte   height;
@@ -50,16 +52,13 @@
   } FTC_SBitRec;
 
 
-  FT_EXPORT_DEF( FT_Error )
-  FTC_SBit_Cache_New( FTC_Manager      manager,
-                      FTC_SBit_Cache  *acache );
+  FT_EXPORT_DEF( FT_Error )  FTC_SBit_Cache_New( FTC_Manager      manager,
+                                                 FTC_SBit_Cache*  acache );
 
-
-  FT_EXPORT_DEF( FT_Error )
-  FTC_SBit_Cache_Lookup( FTC_SBit_Cache   cache,
-                         FTC_Image_Desc*  desc,
-                         FT_UInt          gindex,
-                         FTC_SBit        *sbit );
+  FT_EXPORT_DEF( FT_Error )  FTC_SBit_Cache_Lookup( FTC_SBit_Cache   cache,
+                                                    FTC_Image_Desc*  desc,
+                                                    FT_UInt          gindex,
+                                                    FTC_SBit        *sbit );
 
 
 #ifdef __cplusplus
@@ -69,5 +68,5 @@
 
 #endif /* FTCSBITS_H */
 
-/* END */
 
+/* END */
