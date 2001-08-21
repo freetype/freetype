67750c8fa425a918f39b9145aa5aa494f40fddb3
David Turner <david@freetype.org>
2001-08-21 08:50:45 +0000

    updating the definition of FT_CONFIG_OPTION_USE_MODULE_ERRORS to _undefined_ (source compatibility breaking issue) + updated its comment
    back from holidays, woohoo :-)

--- include/freetype/config/ftoption.h
+++ include/freetype/config/ftoption.h
@@ -77,16 +77,17 @@ FT_BEGIN_HEADER
   /*                                                                       */
   /* Module errors                                                         */
   /*                                                                       */
-  /*   If this macro is set (which is the default), the higher byte of an  */
-  /*   error code gives the module in which the error has occurred, while  */
-  /*   the lower byte is the real error code.                              */
+  /*   If this macro is set (which is _not_ the default), the higher byte  */
+  /*   of an error code gives the module in which the error has occurred,  */
+  /*   while the lower byte is the real error code.                        */
   /*                                                                       */
-  /*   Unsetting this macro makes sense for backwards compatibility; only  */
-  /*   the real error code is emitted, and the higher byte is always zero. */
+  /*   Setting this macro makes sense for debugging purposes only, since   */
+  /*   it would break source compatibility of certain programs that use    */
+  /*   FreeType 2.                                                         */
   /*                                                                       */
   /*   More details can be found in the files ftmoderr.h and fterrors.h.   */
   /*                                                                       */
-#define FT_CONFIG_OPTION_USE_MODULE_ERRORS
+#undef  FT_CONFIG_OPTION_USE_MODULE_ERRORS
 
 
   /*************************************************************************/
