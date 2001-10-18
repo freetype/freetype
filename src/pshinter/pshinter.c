e888167e1f0263cafb14f4ad38785a4600b2eaea
David Turner <david@freetype.org>
2001-10-18 11:38:43 +0000

    adding new postscript hinter

---
+++ src/pshinter/pshinter.c
@@ -0,0 +1,29 @@
+/***************************************************************************/
+/*                                                                         */
+/*  pshinter.c                                                             */
+/*                                                                         */
+/*    FreeType Postscript Hinting module                                   */
+/*                                                                         */
+/*  Copyright 1996-2000 by                                                 */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+
+#include <ft2build.h>
+#include "pshrec.c"  
+#include "pshglob.c"
+#include "pshalgo1.c" 
+#include "pshalgo2.c"
+#include "pshmod.c"
+
+
+/* END */
