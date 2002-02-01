4c40011140a390bf9cd222e0cbe3d21dbbd2e4ba
David Turner <david@freetype.org>
2002-02-01 09:22:39 +0000

    increasing FREETYPE_PATCH to 7

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -35,7 +35,7 @@
   /*                                                                       */
 #define FREETYPE_MAJOR 2
 #define FREETYPE_MINOR 0
-#define FREETYPE_PATCH 5
+#define FREETYPE_PATCH 7
 
 
 #include <ft2build.h>
