9f95babf3e1be13c2f7ecda60938698b7e2da7f6
David Turner <david@freetype.org>
2002-06-07 07:23:06 +0000

    * src/type42/t42drivr.c, src/type42/t42drivr.h, src/type42/t42parse.c,           src/type42/t42parse.h, src/type42/t42objs.h, src/type42/t42objs.c,           src/type42/type42.c:
                updated the Type42 driver by splitting it into several files since
                it makes the code easier to read and maintain. Also fixed the bug
                that prevented the correct display of fonts with "ftview"

---
+++ src/type42/t42drivr.h
@@ -0,0 +1,38 @@
+/***************************************************************************/
+/*                                                                         */
+/*  t42drivr.h                                                             */
+/*                                                                         */
+/*    High-level Type 42 driver interface (specification).                 */
+/*                                                                         */
+/*  Copyright 1996-2001, 2002 by                                           */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+#ifndef __T42DRIVER_H__
+#define __T42DRIVER_H__
+
+
+#include <ft2build.h>
+#include FT_INTERNAL_DRIVER_H
+
+
+FT_BEGIN_HEADER
+
+
+  FT_EXPORT_VAR( const FT_Driver_ClassRec )  t42_driver_class;
+
+
+FT_END_HEADER
+
+#endif /* __T42DRIVER_H__ */
+
+
+/* END */
---
+++ src/type42/type42.c
@@ -0,0 +1,17 @@
+/***************************************************************************/
+/*                                                                         */
+/*  type42c                                                                */
+/*                                                                         */
+/*    FreeType Type 42 driver component                                    */
+/*                                                                         */
+/***************************************************************************/
+
+
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+
+#include <ft2build.h>
+#include "t42objs.c"
+#include "t42parse.c"
+#include "t42drivr.c"
+
+/* END */
