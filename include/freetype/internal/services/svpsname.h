f4c0b921a18ad421a022ee7c1d2449daae502ae0
Werner Lemberg <wl@gnu.org>
2003-09-30 05:39:35 +0000

    * include/freetype/internal/t1types.h: Don't include FT_INTERNAL_OBJECTS_H but FT_INTERNAL_SERVICE_H. * src/truetype/ttobjs.c: Don't include FT_SERVICE_POSTSCRIPT_NAMES_H.

--- include/freetype/internal/services/svpsname.h
+++ include/freetype/internal/services/svpsname.h
@@ -1,36 +1,61 @@
+/***************************************************************************/
+/*                                                                         */
+/*  svpsname.h                                                             */
+/*                                                                         */
+/*    The FreeType glyph PS name services (specification).                 */
+/*                                                                         */
+/*  Copyright 2003 by                                                      */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
 #ifndef __SVPSNAME_H__
 #define __SVPSNAME_H__
 
+
 FT_BEGIN_HEADER
 
-#define  FT_SERVICE_ID_POSTSCRIPT_NAMES  "postscript-names"
 
- /*  Adobe glyph name to unicode value
-  */
+#define FT_SERVICE_ID_POSTSCRIPT_NAMES  "postscript-names"
+
+
+  /*
+   *  Adobe glyph name to unicode value
+   */
   typedef FT_UInt32
   (*PS_Unicode_ValueFunc)( const char*  glyph_name );
 
- /*  Unicode value to Adobe glyph name index. 0xFFFF if not found
-  */
+  /*
+   *  Unicode value to Adobe glyph name index. 0xFFFF if not found
+   */
   typedef FT_UInt
   (*PS_Unicode_Index_Func)( FT_UInt       num_glyphs,
                             const char**  glyph_names,
                             FT_ULong      unicode );
 
- /* Macintosh name id to glyph name, NULL if invalid index
-  */
+  /*
+   *  Macintosh name id to glyph name, NULL if invalid index
+   */
   typedef const char*
   (*PS_Macintosh_Name_Func)( FT_UInt  name_index );
 
- /* Adobe standard string id to glyph name, NULL if invalid index
-  */
+  /*
+   *  Adobe standard string id to glyph name, NULL if invalid index
+   */
   typedef const char*
   (*PS_Adobe_Std_Strings_Func)( FT_UInt  string_index );
 
-
- /* Simple unicode -> glyph index charmap built from font glyph names
-  * table
-  */
+  /*
+   *  Simple unicode -> glyph index charmap built from font glyph names
+   *  table.
+   */
   typedef struct  PS_UniMap_
   {
     FT_UInt  unicode;
@@ -61,6 +86,7 @@ FT_BEGIN_HEADER
   (*PS_Unicodes_CharNextFunc)( PS_Unicodes*  unicodes,
                                FT_ULong      unicode );
 
+
   FT_DEFINE_SERVICE( PsNames )
   {
     PS_Unicode_ValueFunc       unicode_value;
@@ -77,6 +103,8 @@ FT_BEGIN_HEADER
 
  /* */
 
+
 FT_END_HEADER
+
 
 #endif /* __SVPSNAME_H__ */
