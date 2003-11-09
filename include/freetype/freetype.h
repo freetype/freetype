56b886620f7ff65e688e8eea7fcf1c1f5c23c840
Werner Lemberg <wl@gnu.org>
2003-11-09 08:38:13 +0000

    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 7.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -44,7 +44,7 @@
   /*                                                                       */
 #define FREETYPE_MAJOR 2
 #define FREETYPE_MINOR 1
-#define FREETYPE_PATCH 6
+#define FREETYPE_PATCH 7
 
 
 #include <ft2build.h>
