4ad75495f14d483575c2a8cc0ca6b3f43e148a85
Werner Lemberg <wl@gnu.org>
2003-12-22 11:35:36 +0000

    * src/type1/t1load.c (parse_dict): Stop parsing if `eexec' keyword is encountered.

--- src/type1/t1load.c
+++ src/type1/t1load.c
@@ -1527,6 +1527,11 @@
         parser->root.cursor = cur2;
       }
 
+      /* look for `eexec' */
+      else if ( *cur == 'e' && cur + 5 < limit &&
+                ft_strncmp( (char*)cur, "eexec", 5 ) == 0 )
+        break;
+
       /* look for `closefile' which ends the eexec section */
       else if ( *cur == 'c' && cur + 9 < limit &&
                 ft_strncmp( (char*)cur, "closefile", 9 ) == 0 )
