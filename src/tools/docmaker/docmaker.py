ee42805e5f49ffc6c46d9f1045ca2fc63a09f246
David Turner <david@freetype.org>
2004-01-16 14:26:32 +0000

    * src/tools/docmaker/*: updating the DocMaker tool, adding a new         tool named "docbeauty" to beautify the documentation comments         (e.g. convert them to a single block border mode)

--- src/tools/docmaker/docmaker.py
+++ src/tools/docmaker/docmaker.py
@@ -24,48 +24,6 @@ import utils
 import sys, os, time, string, glob, getopt
 
 
-def file_exists( pathname ):
-    """checks that a given file exists"""
-    result = 1
-    try:
-        file = open( pathname, "r" )
-        file.close()
-    except:
-        result = None
-        sys.stderr.write( pathname + " couldn't be accessed\n" )
-
-    return result
-
-
-def make_file_list( args = None ):
-    """builds a list of input files from command-line arguments"""
-
-    file_list = []
-    # sys.stderr.write( repr( sys.argv[1 :] ) + '\n' )
-
-    if not args:
-        args = sys.argv[1 :]
-
-    for pathname in args:
-        if string.find( pathname, '*' ) >= 0:
-            newpath = glob.glob( pathname )
-            newpath.sort()  # sort files -- this is important because
-                            # of the order of files
-        else:
-            newpath = [pathname]
-            
-        file_list.extend( newpath )
-
-    if len( file_list ) == 0:
-        file_list = None
-    else:
-        # now filter the file list to remove non-existing ones
-        file_list = filter( file_exists, file_list )
-    
-    return file_list
-
-
-
 def usage():
     print "\nDocMaker 0.2 Usage information\n"
     print "  docmaker [options] file1 [ file2 ... ]\n"
