b0db0fc2c5c7d87d457abe2bd5a7c759eebb3e66
Werner Lemberg <wl@gnu.org>
2005-03-05 17:05:35 +0000

    * autogen.sh: New script for bootstrapping.
    * README.CVS: New file which documents bootstrapping.
    
    * builds/unix/aclocal.m4, builds/unix/config.guess,
    builds/unix/config.sub, builds/unix/configure,
    builds/unix/ltmain.sh: Removed.

---
+++ autogen.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+run ()
+{
+  echo "running \`$*'"
+  eval $*
+
+  if test $? != 0 ; then
+    echo "error while running \`$*'"
+    exit 1
+  fi
+}
+
+if test ! -f ./builds/unix/configure.ac; then
+  echo "You must be in the same directory as \`autogen.sh'."
+  echo "Bootstrapping doesn't work if srcdir != builddir."
+  exit 1
+fi
+
+cd builds/unix
+
+run aclocal -I .
+run libtoolize --force --copy
+run autoconf
+
+chmod +x mkinstalldirs
+chmod +x install-sh
+
+cd ../..
+
+chmod +x ./configure
+
+# EOF
