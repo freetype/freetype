a6e9ff3d20326d8d590f52643c69d73a2ad542e2
Werner Lemberg <wl@gnu.org>
2005-03-26 22:57:57 +0000

    * builds/amiga/src/base/ftsystem.c (ft_amiga_stream_io): Fix buffer offsets after a large read.

--- builds/amiga/src/base/ftsystem.c
+++ builds/amiga/src/base/ftsystem.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    Amiga-specific FreeType low-level system interface (body).           */
 /*                                                                         */
-/*  Copyright 1996-2001, 2002 by                                           */
+/*  Copyright 1996-2001, 2002, 2005 by                                     */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -349,7 +349,7 @@ Free_VecPooled( APTR  poolHeader,
             ULONG bufsize;
 
             bufsize = ( read_bytes > IOBUF_SIZE ) ? IOBUF_SIZE : read_bytes;
-            sysfile->iobuf_end = offset + read_bytes - bufsize;
+            sysfile->iobuf_end = offset + read_bytes;
             sysfile->iobuf_start = sysfile->iobuf_end - bufsize;
             CopyMem( &buffer[read_bytes - bufsize] , sysfile->iobuf, bufsize );
           }
