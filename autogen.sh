f79b3d0244c62dd2150fb1f68ffbd56e12597c63
Werner Lemberg <wl@gnu.org>
2005-06-04 23:04:30 +0000

    Add license.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Copyright 2005 by
+# David Turner, Robert Wilhelm, and Werner Lemberg.
+#
+# This file is part of the FreeType project, and may only be used, modified,
+# and distributed under the terms of the FreeType project license,
+# LICENSE.TXT.  By continuing to use, modify, or distribute this file you
+# indicate that you have read the license and understand and accept it
+# fully.
+
 run ()
 {
   echo "running \`$*'"
