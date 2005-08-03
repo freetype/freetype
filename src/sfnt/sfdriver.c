b2d65d75bd2bdb1c6392131f0ea58bfb52ac7d2d
Werner Lemberg <wl@gnu.org>
2005-08-03 21:21:11 +0000

    * src/sfnt/sfdriver.c (sfnt_interface) [FT_OPTIMIZE_MEMORY]: Reactivate pointers to tt_find_sbit_image and tt_load_sbit_metrics to make X work again.

--- src/sfnt/sfdriver.c
+++ src/sfnt/sfdriver.c
@@ -367,8 +367,13 @@
     /* see `ttsbit.h' and `sfnt.h' */
     tt_face_set_sbit_strike,
     tt_face_load_sbit_strikes,
-    0 /* tt_find_sbit_image */,
-    0 /* tt_load_sbit_metrics */,
+#ifdef FT_OPTIMIZE_MEMORY
+    0,
+    0,
+#else
+    tt_find_sbit_image,
+    tt_load_sbit_metrics,
+#endif
     tt_face_load_sbit_image,
     tt_face_free_sbit_strikes,
 
