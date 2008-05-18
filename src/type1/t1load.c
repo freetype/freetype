16dc51feaec4086a8b93292804223d6b4e93def9
Werner Lemberg <wl@gnu.org>
2008-05-18 17:36:15 +0000

    * src/base/ftdebug.c (FT_Message, FT_Panic): Send output to stderr. This fixes Savannah bug #23280.
    * docs/CHANGES: Updated.
    
    Some formatting.

--- src/type1/t1load.c
+++ src/type1/t1load.c
@@ -1397,9 +1397,9 @@
         FT_Byte*  temp;
 
 
-        /* some fonts define empty subr records. this is not totally
-         * compliant to the spec (which says they should at least
-         * contain a 'return') but we support them */
+        /* some fonts define empty subr records -- this is not totally */
+        /* compliant to the specification (which says they should at   */
+        /* least contain a `return'), but we support them anyway       */
         if ( size < face->type1.private_dict.lenIV )
         {
           error = T1_Err_Invalid_File_Format;
