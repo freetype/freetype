5df5dbb722cf008b3f617448d8d28fde4d725556
Werner Lemberg <wl@gnu.org>
2008-06-22 13:40:08 +0000

    Enable access to the various dropout rules of the B&W rasterizer. Pass dropout rules from the TT bytecode interpreter to the rasterizer; temporarily this is enabled only if `USE_SCAN_CONVERSION_RULES' is defined.
    * include/freetype/ftimage.h (FT_OUTLINE_SMART_DROPOUTS,
    FT_OUTLINE_EXCLUDE_STUBS): New flags for for FT_Outline.
    
    * src/raster/ftraster.c (Vertical_Sweep_Drop, Horizontal_Sweep_Drop,
    Horizontal_Gray_Sweep_Drop): Use same mode numbers as given in the
    OpenType specification.
    Fix mode 4 computation.
    (Render_Glyph, Render_Gray_Glyph): Handle new outline flags.
    
    * src/truetype/ttgload.c (TT_Load_Glyph)
    [USE_SCAN_CONVERSION_RULES]: Convert scan conversion mode to
    FT_OUTLINE_XXX flags.
    
    * src/truetype/ttinterp.c (Ins_SCANCTRL): Enable ppem check.

--- src/truetype/ttgload.c
+++ src/truetype/ttgload.c
@@ -557,10 +557,10 @@
       FT_Stream  stream = loader->stream;
 
 
-      /* we must undo the FT_FRAME_ENTER in order to point to the */
-      /* composite instructions, if we find some.               */
-      /* we will process them later...                          */
-      /*                                                        */
+      /* we must undo the FT_FRAME_ENTER in order to point */
+      /* to the composite instructions, if we find some.   */
+      /* We will process them later.                       */
+      /*                                                   */
       loader->ins_pos = (FT_ULong)( FT_STREAM_POS() +
                                     p - limit );
     }
@@ -1957,6 +1957,37 @@
         if ( ( face->header.Flags & 2 ) == 0 && loader.pp1.x )
           FT_Outline_Translate( &glyph->outline, -loader.pp1.x, 0 );
       }
+
+#ifdef TT_USE_BYTECODE_INTERPRETER
+
+      if ( loader.exec->GS.scan_control )
+      {
+        /* convert scan conversion mode to FT_OUTLINE_XXX flags */
+        switch ( loader.exec->GS.scan_type )
+        {
+        case 0: /* simple drop-outs including stubs */
+          glyph->outline.flags |= FT_OUTLINE_INCLUDE_STUBS;
+          break;
+        case 1: /* simple drop-outs excluding stubs */
+          /* nothing; it's the default rendering mode */
+          break;
+        case 4: /* smart drop-outs including stubs */
+          glyph->outline.flags |= FT_OUTLINE_SMART_DROPOUTS |
+                                  FT_OUTLINE_INCLUDE_STUBS;
+          break;
+        case 5: /* smart drop-outs excluding stubs  */
+          glyph->outline.flags |= FT_OUTLINE_SMART_DROPOUTS;
+          break;
+
+        default: /* no drop-out control */
+          glyph->outline.flags |= FT_OUTLINE_IGNORE_DROPOUTS;
+          break;
+        }
+      }
+      else 
+        glyph->outline.flags |= FT_OUTLINE_IGNORE_DROPOUTS;
+
+#endif /* TT_USE_BYTECODE_INTERPRETER */
 
       compute_glyph_metrics( &loader, glyph_index );
     }
