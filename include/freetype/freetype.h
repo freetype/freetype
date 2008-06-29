be54a68c9dfd89a2d5c88b1639821211e922e539
Werner Lemberg <wl@gnu.org>
2008-06-29 07:43:41 +0000

    * Version 2.3.7 released. =========================
    Tag sources with `VER-2-3-7'.
    
    * docs/CHANGES, docs/VERSION.DLL: Update documentation and bump
    version number to 2.3.7.
    
    * README, Jamfile (RefDoc), builds/win32/visualc/index.html,
    builds/win32/visualc/freetype.dsp,
    builds/win32/visualc/freetype.vcproj,
    builds/win32/visualce/index.html,
    builds/win32/visualce/freetype.dsp,
    builds/win32/visualce/freetype.vcproj: s/2.3.6/2.3.7/, s/236/237/.
    
    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 7.
    
    * builds/unix/configure.raw (version_info): Set to 9:18:3.
    
    * docs/release: Updated.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -3611,7 +3611,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  3
-#define FREETYPE_PATCH  6
+#define FREETYPE_PATCH  7
 
 
   /*************************************************************************/
