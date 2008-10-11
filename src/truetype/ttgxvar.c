0192230ea206e7bf4316483103c308fda177e874
Werner Lemberg <wl@gnu.org>
2008-10-11 06:00:15 +0000

    (important) formatting

--- src/truetype/ttgxvar.c
+++ src/truetype/ttgxvar.c
@@ -907,11 +907,12 @@
     {
       for ( i = 0;
             i < num_coords && blend->normalizedcoords[i] == coords[i];
-            ++i );
-        if ( i == num_coords )
-          manageCvt = mcvt_retain;
-        else
-          manageCvt = mcvt_load;
+            ++i )
+        ;
+      if ( i == num_coords )
+        manageCvt = mcvt_retain;
+      else
+        manageCvt = mcvt_load;
 
       /* If we don't change the blend coords then we don't need to do  */
       /* anything to the cvt table.  It will be correct.  Otherwise we */
