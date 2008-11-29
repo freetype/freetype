b2cb6ce6c4d7d7bd25818d22e60378574692ce8d
Werner Lemberg <wl@gnu.org>
2008-11-29 09:32:12 +0000

    * src/type1/t1load.c (mm_axis_unmap): `blend_points' is FT_Fixed*, whereas `design_points' is FT_Long*.  Therefore, return blend rather than design points.

--- src/type1/t1load.c
+++ src/type1/t1load.c
@@ -230,7 +230,7 @@
 
 
     if ( ncv <= axismap->blend_points[0] )
-      return axismap->design_points[0];
+      return axismap->blend_points[0];
 
     for ( j = 1; j < axismap->num_points; ++j )
     {
@@ -249,7 +249,7 @@
       }
     }
 
-    return axismap->design_points[axismap->num_points - 1];
+    return axismap->blend_points[axismap->num_points - 1];
   }
 
 
