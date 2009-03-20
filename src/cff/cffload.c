0545ec1ca36b27cb928128870a83e5f668980bc5
Werner Lemberg <wl@gnu.org>
2009-03-20 06:49:10 +0100

    Protect against invalid SID values in CFFs.
    Problem reported by Tavis Ormandy <taviso@google.com>.
    
    * src/cff/cffload.c (cff_charset_load): Reject SID values larger
    than 64999.

--- src/cff/cffload.c
+++ src/cff/cffload.c
@@ -842,7 +842,20 @@
             goto Exit;
 
           for ( j = 1; j < num_glyphs; j++ )
-            charset->sids[j] = FT_GET_USHORT();
+          {
+            FT_UShort sid = FT_GET_USHORT();
+
+
+            /* this constant is given in the CFF specification */
+            if ( sid < 65000 )
+              charset->sids[j] = sid;
+            else
+            {
+              FT_ERROR(( "cff_charset_load:"
+                         " invalid SID value %d set to zero\n", sid ));
+              charset->sids[j] = 0;
+            }
+          }
 
           FT_FRAME_EXIT();
         }
@@ -873,6 +886,20 @@
             {
               if ( FT_READ_BYTE( nleft ) )
                 goto Exit;
+            }
+
+            /* check whether the range contains at least one valid glyph; */
+            /* the constant is given in the CFF specification             */
+            if ( glyph_sid >= 65000 ) {
+              FT_ERROR(( "cff_charset_load: invalid SID range\n" ));
+              error = CFF_Err_Invalid_File_Format;
+              goto Exit;
+            }
+
+            /* try to rescue some of the SIDs if `nleft' is too large */
+            if ( nleft > 65000 - 1 || glyph_sid >= 65000 - nleft ) {
+              FT_ERROR(( "cff_charset_load: invalid SID range trimmed\n" ));
+              nleft = 65000 - 1 - glyph_sid;
             }
 
             /* Fill in the range of sids -- `nleft + 1' glyphs. */
