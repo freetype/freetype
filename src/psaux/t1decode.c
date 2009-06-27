6bb34880e87235dc0f51d2bbf1c5f48cd7cd407e
Werner Lemberg <wl@gnu.org>
2009-06-28 00:43:37 +0200

    Thinko.
    * src/psaux/t1decode.c (t1operator_seac)
    [FT_CONFIG_OPTION_INCREMENTAL]: Test for existence of incremental
    interface.

--- src/psaux/t1decode.c
+++ src/psaux/t1decode.c
@@ -194,6 +194,9 @@
 #endif
     FT_Vector    left_bearing, advance;
 
+#ifdef FT_CONFIG_OPTION_INCREMENTAL
+    T1_Face      face  = (T1_Face)decoder->builder.face;
+#endif     
 
     /* seac weirdness */
     adx += decoder->builder.left_bearing.x;
@@ -208,17 +211,18 @@
     }
 
 #ifdef FT_CONFIG_OPTION_INCREMENTAL
-
-    /* the caller must handle the font encoding also */
-    bchar_index = bchar;
-    achar_index = achar;
-
-#else
-
-    bchar_index = t1_lookup_glyph_by_stdcharcode( decoder, bchar );
-    achar_index = t1_lookup_glyph_by_stdcharcode( decoder, achar );
-
+    if ( face->root.internal->incremental_interface )
+    {
+      /* the caller must handle the font encoding also */
+      bchar_index = bchar;
+      achar_index = achar;
+    }
+    else
 #endif
+    {
+      bchar_index = t1_lookup_glyph_by_stdcharcode( decoder, bchar );
+      achar_index = t1_lookup_glyph_by_stdcharcode( decoder, achar );
+    }
 
     if ( bchar_index < 0 || achar_index < 0 )
     {
