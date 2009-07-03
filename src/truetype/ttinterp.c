009c39c1d99da8248215a3421f61981fbb856b51
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2009-07-03 18:01:34 +0900

    truetype: Cast the project vector to 32-bit for LP64 system.

--- src/truetype/ttinterp.c
+++ src/truetype/ttinterp.c
@@ -2194,7 +2194,7 @@
     FT_ASSERT( !CUR.face->unpatented_hinting );
 #endif
 
-    return TT_DotFix14( dx, dy,
+    return TT_DotFix14( (FT_UInt32)dx, (FT_UInt32)dy,
                         CUR.GS.projVector.x,
                         CUR.GS.projVector.y );
   }
@@ -2220,7 +2220,7 @@
   Dual_Project( EXEC_OP_ FT_Pos  dx,
                          FT_Pos  dy )
   {
-    return TT_DotFix14( dx, dy,
+    return TT_DotFix14( (FT_UInt32)dx, (FT_UInt32)dy,
                         CUR.GS.dualVector.x,
                         CUR.GS.dualVector.y );
   }
