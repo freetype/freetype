d200982d8b35be32c75f2af9c9b175af190d5a9d
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2009-08-01 00:30:17 +0900

    psaux: Use size_t variable to pass the buffer size.

--- include/freetype/internal/psaux.h
+++ include/freetype/internal/psaux.h
@@ -360,7 +360,7 @@ FT_BEGIN_HEADER
     FT_Error
     (*to_bytes)( PS_Parser  parser,
                  FT_Byte*   bytes,
-                 FT_Long    max_bytes,
+                 FT_Offset  max_bytes,
                  FT_Long*   pnum_bytes,
                  FT_Bool    delimiters );
 
