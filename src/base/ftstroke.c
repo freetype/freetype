fc58155afc5213480f331990c28e83105f20a371
Werner Lemberg <wl@gnu.org>
2010-01-05 20:00:35 +0100

    Apply reports from clang static analyzer.
    * src/lzw/ftlzw.c (ft_lzw_file_init), src/base/ftstroke.c
    (FT_Stroker_ParseOutline), src/base/ftsynth.c
    (FT_GlyphSlot_Embolden): Remove dead code.
    
    * src/base/ftpatent.c (_tt_check_patents_in_table): Initialize
    `offset_i'.

--- src/base/ftstroke.c
+++ src/base/ftstroke.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    FreeType path stroker (body).                                        */
 /*                                                                         */
-/*  Copyright 2002, 2003, 2004, 2005, 2006, 2008, 2009 by                  */
+/*  Copyright 2002, 2003, 2004, 2005, 2006, 2008, 2009, 2010 by            */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -1736,13 +1736,10 @@
         }
         else
         {
-          /* if both first and last points are conic,         */
-          /* start at their middle and record its position    */
-          /* for closure                                      */
+          /* if both first and last points are conic, */
+          /* start at their middle                    */
           v_start.x = ( v_start.x + v_last.x ) / 2;
           v_start.y = ( v_start.y + v_last.y ) / 2;
-
-          v_last = v_start;
         }
         point--;
         tags--;
