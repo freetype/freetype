1fc440b222d60ab7927350f583b8cfa01b2c77db
Werner Lemberg <wl@gnu.org>
2010-01-05 13:39:37 +0100

    Typo.

--- src/type1/t1gload.c
+++ src/type1/t1gload.c
@@ -289,7 +289,7 @@
 
 #ifdef FT_CONFIG_OPTION_INCREMENTAL
     if ( glyph_index >= (FT_UInt)face->root.num_glyphs &&
-         face->root.internal->incremental_interface    )
+         !face->root.internal->incremental_interface   )
 #else
     if ( glyph_index >= (FT_UInt)face->root.num_glyphs )
 #endif /* FT_CONFIG_OPTION_INCREMENTAL */
