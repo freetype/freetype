b69c6ac0118844258e9078be55281c57449de791
Werner Lemberg <wl@gnu.org>
2010-07-18 06:20:09 +0200

    * Version 2.4.1 released. =========================
    Tag sources with `VER-2-4-1'.
    
    * docs/CHANGES: Updated.
    
    * docs/VERSION.DLL: Update documentation and bump version number to
    2.4.1.
    
    * README, Jamfile (RefDoc),
    builds/win32/vc2005/freetype.vcproj, builds/win32/vc2005/index.html,
    builds/win32/vc2008/freetype.vcproj, builds/win32/vc2008/index.html,
    builds/win32/visualc/freetype.dsp,
    builds/win32/visualc/freetype.vcproj,
    builds/win32/visualc/index.html, builds/win32/visualce/freetype.dsp,
    builds/win32/visualce/freetype.vcproj,
    builds/win32/visualce/index.html,
    builds/wince/vc2005-ce/freetype.vcproj,
    builds/wince/vc2005-ce/index.html,
    builds/wince/vc2008-ce/freetype.vcproj,
    builds/wince/vc2008-ce/index.html: s/2.4.0/2.4.1/, s/240/241/.
    
    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 1.
    
    * builds/unix/configure.raw (version_info): Set to 11:1:5.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -3774,7 +3774,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  4
-#define FREETYPE_PATCH  0
+#define FREETYPE_PATCH  1
 
 
   /*************************************************************************/
