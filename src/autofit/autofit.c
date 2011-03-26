f3c57917f09ef879fc34d1525a6f83e4e7507d0f
Werner Lemberg <wl@gnu.org>
2011-03-26 09:03:32 +0100

    Copyright.

--- src/autofit/autofit.c
+++ src/autofit/autofit.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    Auto-fitter module (body).                                           */
 /*                                                                         */
-/*  Copyright 2003, 2004, 2005, 2006, 2007 by                              */
+/*  Copyright 2003-2007, 2011 by                                           */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
