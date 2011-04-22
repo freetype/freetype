233f9d5dd28e4e21db8d26f096b81c0cbc3a9626
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2011-04-22 10:14:07 +0900

    [autofit] Add more Indic scripts with hanging baseline.
    * src/autofit/afindic.c (af_indic_uniranges): Tibetan, Limbu,
    Sundanese, Meetei Mayak, Syloti Nagri and Sharada scripts are
    added.

--- src/autofit/afindic.c
+++ src/autofit/afindic.c
@@ -84,6 +84,12 @@
     AF_UNIRANGE_REC( 0x0100UL, 0xFFFFUL ),  /* why this? */
 #endif
     AF_UNIRANGE_REC( 0x0900UL, 0x0DFFUL),    /* Indic Range */
+    AF_UNIRANGE_REC( 0x0F00UL, 0x0FFFUL),    /* Tibetan */
+    AF_UNIRANGE_REC( 0x1900UL, 0x194FUL),    /* Limbu */
+    AF_UNIRANGE_REC( 0x1B80UL, 0x1BBFUL),    /* Sundanese */
+    AF_UNIRANGE_REC( 0x1C80UL, 0x1CDFUL),    /* Meetei Mayak */
+    AF_UNIRANGE_REC( 0xA800UL, 0xA82FUL),    /* Syloti Nagri */
+    AF_UNIRANGE_REC( 0x11800UL, 0x118DFUL),  /* Sharada */
     AF_UNIRANGE_REC(      0UL,      0UL)
   };
 
