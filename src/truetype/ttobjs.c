86b7b2f614873386a3bd56be77c8ba37a69a2a02
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2011-04-26 03:46:12 +0900

    [truetype] Revert the reordering of trickyness checking.
    Trickyness check by the family name is faster than that by
    the checksum.

--- src/truetype/ttobjs.c
+++ src/truetype/ttobjs.c
@@ -332,15 +332,15 @@
     if ( !face )
       return FALSE;
 
+    /* For first, check the face name for quick check. */
+    if ( face->family_name                               &&
+         tt_check_trickyness_family( face->family_name ) )
+      return TRUE;
+
     /* Type42 fonts may lack `name' tables, we thus try to identify */
     /* tricky fonts by checking the checksums of Type42-persistent  */
     /* sfnt tables (`cvt', `fpgm', and `prep').                     */
     if ( tt_check_trickyness_sfnt_ids( (TT_Face)face ) )
-      return TRUE;
-
-    /* Secondary, check the face name. */
-    if ( face->family_name                               &&
-         tt_check_trickyness_family( face->family_name ) )
       return TRUE;
 
     return FALSE;
