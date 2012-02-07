e343e87d4f2914cf1a9f55c07d79530c4c85e322
Werner Lemberg <wl@gnu.org>
2012-02-07 09:40:11 +0100

    [autofit] Harmonize function arguments.
    * src/autofit/afloader.c, src/autofit/afloader.h: Use `FT_Int32' for
    `load_flags'.

--- src/autofit/afloader.c
+++ src/autofit/afloader.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    Auto-fitter glyph loading routines (body).                           */
 /*                                                                         */
-/*  Copyright 2003-2009, 2011 by                                           */
+/*  Copyright 2003-2009, 2011-2012 by                                      */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -480,7 +480,7 @@
   af_loader_load_glyph( AF_Loader  loader,
                         FT_Face    face,
                         FT_UInt    gindex,
-                        FT_UInt32  load_flags )
+                        FT_Int32   load_flags )
   {
     FT_Error      error;
     FT_Size       size = face->size;
