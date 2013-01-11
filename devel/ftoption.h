e4ecce3bfe464cf56a3838db945ca0e528e5555f
Werner Lemberg <wl@gnu.org>
2013-01-11 09:02:22 +0100

    Disable FT_CONFIG_OPTION_OLD_INTERNALS.
    After the next release we are going to remove the code completely.
    
    * devel/ftoption.h, include/freetype/config/ftoption.h
    (FT_CONFIG_OPTION_OLD_INTERNALS): Comment out.
    * docs/CHANGES: Document it.

--- devel/ftoption.h
+++ devel/ftoption.h
@@ -783,7 +783,7 @@ FT_BEGIN_HEADER
    * is recommended to disable the macro since it reduces the library's code
    * size and activates a few memory-saving optimizations as well.
    */
-#define FT_CONFIG_OPTION_OLD_INTERNALS
+/* #define FT_CONFIG_OPTION_OLD_INTERNALS */
 
 
   /*
