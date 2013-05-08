cecf93ef90c660a8b0b45e5adbbfb5ea443fb6b9
Werner Lemberg <wl@gnu.org>
2013-05-08 11:39:16 +0200

    * Version 2.4.12 released. ==========================
    Tag sources with `VER-2-4-12'.
    
    * docs/VERSION.DLL: Update documentation and bump version number to
    2.4.12.
    
    * README, Jamfile (RefDoc),
    builds/win32/vc2005/freetype.vcproj, builds/win32/vc2005/index.html,
    builds/win32/vc2008/freetype.vcproj, builds/win32/vc2008/index.html,
    builds/win32/vc2010/freetype.vcxproj, builds/win32/vc2010/index.html,
    builds/win32/visualc/freetype.dsp,
    builds/win32/visualc/freetype.vcproj,
    builds/win32/visualc/index.html, builds/win32/visualce/freetype.dsp,
    builds/win32/visualce/freetype.vcproj,
    builds/win32/visualce/index.html,
    builds/wince/vc2005-ce/freetype.vcproj,
    builds/wince/vc2005-ce/index.html,
    builds/wince/vc2008-ce/freetype.vcproj,
    builds/wince/vc2008-ce/index.html: s/2.4.11/2.4.12/, s/2411/2412/.
    
    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 12.
    
    * builds/unix/configure.raw (version_info): Set to 16:1:10.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -3882,7 +3882,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  4
-#define FREETYPE_PATCH  11
+#define FREETYPE_PATCH  12
 
 
   /*************************************************************************/
