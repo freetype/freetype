135c3faebb96f8f550bd4f318716f2e1e095a969
Dave Arnold <darnold@adobe.com>
2014-02-28 07:42:42 +0100

    Fix Savannah bug #41697, part 2.
    * src/cff/cf2ft.c (cf2_initLocalRegionBuffer,
    cf2_initGlobalRegionBuffer): It is possible for a charstring to call
    a subroutine if no subroutines exist.  This is an error but should
    not trigger an assert.  Split the assert to account for this.

--- src/cff/cf2ft.c
+++ src/cff/cf2ft.c
@@ -521,13 +521,15 @@
                               CF2_UInt      idx,
                               CF2_Buffer    buf )
   {
-    FT_ASSERT( decoder && decoder->globals );
+    FT_ASSERT( decoder );
 
     FT_ZERO( buf );
 
     idx += decoder->globals_bias;
     if ( idx >= decoder->num_globals )
       return TRUE;     /* error */
+
+    FT_ASSERT( decoder->globals );
 
     buf->start =
     buf->ptr   = decoder->globals[idx];
@@ -594,13 +596,15 @@
                              CF2_UInt      idx,
                              CF2_Buffer    buf )
   {
-    FT_ASSERT( decoder && decoder->locals );
+    FT_ASSERT( decoder );
 
     FT_ZERO( buf );
 
     idx += decoder->locals_bias;
     if ( idx >= decoder->num_locals )
       return TRUE;     /* error */
+
+    FT_ASSERT( decoder->locals );
 
     buf->start =
     buf->ptr   = decoder->locals[idx];
