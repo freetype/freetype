44accb9e2e5b00696cf50a869b68afa2ce3dd389
Werner Lemberg <wl@gnu.org>
2016-02-08 19:51:40 +0100

    Version 2.6.3 released.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -4172,7 +4172,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  6
-#define FREETYPE_PATCH  2
+#define FREETYPE_PATCH  3
 
 
   /*************************************************************************/
