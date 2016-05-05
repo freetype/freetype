0b3cb8a8afb15247ee55477f73e25e0ca3038e5b
Werner Lemberg <wl@gnu.org>
2016-05-05 12:28:48 +0200

    [cff, truetype] Fix logic for `FT_Property_Set'.
    Otherwise some properties could be set to arbitrary values, which is
    harmless, but querying could give wrong positive results.
    
    * src/cff/cffdrivr.c (cff_property_set) [hinting-engine],
    * src/truetype/ttdriver.c (tt_property_set) [interpreter-version]:
    Only allow defined values.

--- src/truetype/ttdriver.c
+++ src/truetype/ttdriver.c
@@ -72,12 +72,14 @@
       FT_UInt*  interpreter_version = (FT_UInt*)value;
 
 
-#ifndef TT_CONFIG_OPTION_SUBPIXEL_HINTING
-      if ( *interpreter_version != TT_INTERPRETER_VERSION_35 )
-        error = FT_ERR( Unimplemented_Feature );
-      else
+      if ( *interpreter_version == TT_INTERPRETER_VERSION_35
+#ifdef TT_CONFIG_OPTION_SUBPIXEL_HINTING
+           || *interpreter_version == TT_INTERPRETER_VERSION_38
 #endif
+         )
         driver->interpreter_version = *interpreter_version;
+      else
+        error = FT_ERR( Unimplemented_Feature );
 
       return error;
     }
