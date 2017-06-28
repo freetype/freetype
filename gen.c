8e514f4ada3724da8a800be83547a539405c8879
Arvinder Bhathal <arvinder.bhathal@gmail.com>
2017-06-28 04:46:56 -0400

    Begin to consolidate files

---
+++ gen.c
@@ -0,0 +1,14 @@
+// consolidate code into this file 
+
+#include "bitmap.h"
+#include <dlfcn.h>
+
+int main(int argc, char const *argv[])
+{
+  if(argc != 5)
+  {
+    printf("Usage: %s <base ft.so dir> <test ft.so dir> <font dir> <char size>", argv[0]);
+  }
+  return 0;
+
+}
