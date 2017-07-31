9a0220875a59efac2cc143e259c59b040185404e
Kushal K S V S <kkushal32@gmail.com>
2017-07-30 22:44:23 -0700

    Removing files

--- tests/make_bitmap/runme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc example1.c bitmap.c murmur3.c -Wall -I /usr/local/include/freetype2/ -lfreetype -o example1 
-gcc example2.c bitmap.c murmur3.c -Wall -I /usr/local/include/freetype2/ -lfreetype -o example2
-
