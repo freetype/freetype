782336ab3121888f003f8569cf8deb3ed9009ceb
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-09-11 12:04:09 +0900

    builds/compiler/16bit.h: new generic header, to avoid greater-than-int enumarator.

---
+++ builds/compiler/16bit.h
@@ -0,0 +1,7 @@
+#ifndef FT_IMAGE_TAG
+#define FT_IMAGE_TAG( value, _x1, _x2, _x3, _x4 ) value
+#endif /* FT_IMAGE_TAG */
+
+#ifndef FT_ENC_TAG
+#define FT_ENC_TAG( value, a, b, c, d ) value
+#endif /* FT_ENC_TAG */
