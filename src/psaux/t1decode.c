d813b5da5994807fe2357eddae95bd6642e3e7bb
Ewald Hew <ewaldhew@gmail.com>
2017-09-25 08:04:09 +0200

    Extend Adobe interpreter (seac).
    This concludes the changes needed to add Type 1 support.
    
    * src/psaux/psintrp.c: Update includes.
    (cf2_interpT2CharString) <cf2_escSEAC>: Implement this similarly to
    implied seac for CFF.
    
    * src/psaux/t1decode.c (t1_lookup_glyph_by_stdcharcode_ps): New
    function to look up the glyph index.
    
    * src/psaux/psft.c (cf2_getT1SeacComponent,
    cf2_freeT1SeacComponent): New functions to get the charstrings for
    seac components.
    
    * src/psaux/t1decode.h, src/psaux/psft.h: Update declarations.

--- src/psaux/t1decode.c
+++ src/psaux/t1decode.c
@@ -112,6 +112,55 @@
   /*************************************************************************/
   /*                                                                       */
   /* <Function>                                                            */
+  /*    t1_lookup_glyph_by_stdcharcode_ps                                  */
+  /*                                                                       */
+  /* <Description>                                                         */
+  /*    Looks up a given glyph by its StandardEncoding charcode.  Used to  */
+  /*    implement the SEAC Type 1 operator in the Adobe engine             */
+  /*                                                                       */
+  /* <Input>                                                               */
+  /*    face     :: The current face object.                               */
+  /*                                                                       */
+  /*    charcode :: The character code to look for.                        */
+  /*                                                                       */
+  /* <Return>                                                              */
+  /*    A glyph index in the font face.  Returns -1 if the corresponding   */
+  /*    glyph wasn't found.                                                */
+  /*                                                                       */
+  FT_LOCAL_DEF( FT_Int )
+  t1_lookup_glyph_by_stdcharcode_ps( PS_Decoder*  decoder,
+                                     FT_Int       charcode )
+  {
+    FT_UInt             n;
+    const FT_String*    glyph_name;
+    FT_Service_PsCMaps  psnames = decoder->psnames;
+
+
+    /* check range of standard char code */
+    if ( charcode < 0 || charcode > 255 )
+      return -1;
+
+    glyph_name = psnames->adobe_std_strings(
+                   psnames->adobe_std_encoding[charcode]);
+
+    for ( n = 0; n < decoder->num_glyphs; n++ )
+    {
+      FT_String*  name = (FT_String*)decoder->glyph_names[n];
+
+
+      if ( name                               &&
+           name[0] == glyph_name[0]           &&
+           ft_strcmp( name, glyph_name ) == 0 )
+        return (FT_Int)n;
+    }
+
+    return -1;
+  }
+
+
+  /*************************************************************************/
+  /*                                                                       */
+  /* <Function>                                                            */
   /*    t1_lookup_glyph_by_stdcharcode                                     */
   /*                                                                       */
   /* <Description>                                                         */
