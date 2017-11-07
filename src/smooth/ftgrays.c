ab6fe0768fd2a319d73adfd6cc1c8338bf2e669c
Alexei Podtelezhnikov <apodtele@gmail.com>
2017-11-06 21:34:56 -0500

    * src/smooth/ftgrays.c (gray_set_cell): Fix uninitialized variables.

--- src/smooth/ftgrays.c
+++ src/smooth/ftgrays.c
@@ -582,8 +582,8 @@ typedef ptrdiff_t  FT_PtrDist;
     if ( ex < ras.min_ex )
       ex = ras.min_ex - 1;
 
-    /* record the current one if it is substantial and valid */
-    if ( ( ras.area || ras.cover ) && !ras.invalid )
+    /* record the current one if it is valid and substantial */
+    if ( !ras.invalid && ( ras.area || ras.cover ) )
       gray_record_cell( RAS_VAR );
 
     ras.area  = 0;
