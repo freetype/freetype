86bc8a95056c97a810986434a3f268cbe67f2902
Werner Lemberg <wl@gnu.org>
2018-05-01 20:27:24 +0200

    * Version 2.9.1 released. =========================
    Tag sources with `VER-2-9-1'.
    
    * docs/VERSION.TXT: Add entry for version 2.9.1.
    * docs/CHANGES: Updated.
    
    * README, Jamfile (RefDoc), builds/windows/vc2005/freetype.vcproj,
    src/base/ftver.rc, builds/windows/vc2005/index.html,
    builds/windows/vc2008/freetype.vcproj,
    builds/windows/vc2008/index.html,
    builds/windows/vc2010/freetype.vcxproj,
    builds/windows/vc2010/index.html,
    builds/windows/visualc/freetype.dsp,
    builds/windows/visualc/freetype.vcproj,
    builds/windows/visualc/index.html,
    builds/windows/visualce/freetype.dsp,
    builds/windows/visualce/freetype.vcproj,
    builds/windows/visualce/index.html,
    builds/wince/vc2005-ce/freetype.vcproj,
    builds/wince/vc2005-ce/index.html,
    builds/wince/vc2008-ce/freetype.vcproj,
    builds/wince/vc2008-ce/index.html: s/2.9/2.9.1/, s/29/291/.
    
    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 1.
    
    * builds/unix/configure.raw (version_info): Set to 22:1:16.
    * CMakeLists.txt (VERSION_PATCH): Set to 1.
    
    * include/freetype/ftgasp.h: Use FT_BEGIN_HEADER and FT_END_HEADER.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -4557,7 +4557,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  9
-#define FREETYPE_PATCH  0
+#define FREETYPE_PATCH  1
 
 
   /*************************************************************************/
