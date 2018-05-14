b1aef3269f507d985f6f2482d710643adcfb0bb6
Parth Wazurkar <parthwazurkar@gmail.com>
2018-05-14 22:22:57 +0530

    [gf] Added gf.c

---
+++ src/gf/gf.c
@@ -0,0 +1,23 @@
+/***************************************************************************/
+/*                                                                         */
+/*  gf.c                                                                   */
+/*                                                                         */
+/*    FreeType font driver for TeX's GF FONT files                         */
+/*                                                                         */
+/*  Copyright 1996-2018 by                                                 */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+
+#include <ft2build.h>
+
+
+/* END */
