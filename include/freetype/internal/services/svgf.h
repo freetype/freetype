fcef64dde7e0f6a7002f6ceefadaf2b296e5f3c3
Parth Wazurkar <parthwazurkar@gmail.com>
2018-07-16 23:45:48 +0530

    [gf] Register gf services.

---
+++ include/freetype/internal/services/svgf.h
@@ -0,0 +1,40 @@
+/****************************************************************************
+ *
+ * svgf.h
+ *
+ *   The FreeType GF services (specification).
+ *
+ * Copyright 2003-2018 by
+ * David Turner, Robert Wilhelm, and Werner Lemberg.
+ *
+ * This file is part of the FreeType project, and may only be used,
+ * modified, and distributed under the terms of the FreeType project
+ * license, LICENSE.TXT.  By continuing to use, modify, or distribute
+ * this file you indicate that you have read the license and
+ * understand and accept it fully.
+ *
+ */
+
+
+#ifndef SVGF_H_
+#define SVGF_H_
+
+
+#include FT_INTERNAL_SERVICE_H
+
+
+FT_BEGIN_HEADER
+
+
+#define FT_SERVICE_ID_GF  "gf"
+
+  /* */
+
+
+FT_END_HEADER
+
+
+#endif /* SVBDF_H_ */
+
+
+/* END */
