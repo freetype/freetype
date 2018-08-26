ae3afbc47184db5c7753318b36237af261c66b09
Werner Lemberg <wl@gnu.org>
2018-08-26 15:40:16 +0200

    * src/truetype/ttobjs.c (tt_size_read_bytecode): Trace CVT values.

--- src/truetype/ttobjs.c
+++ src/truetype/ttobjs.c
@@ -1174,10 +1174,16 @@
       TT_Face  face = (TT_Face)size->root.face;
 
 
-      /* Scale the cvt values to the new ppem.          */
-      /* We use by default the y ppem to scale the CVT. */
+      /* Scale the cvt values to the new ppem.            */
+      /* By default, we use the y ppem value for scaling. */
+      FT_TRACE6(( "CVT values:\n" ));
       for ( i = 0; i < size->cvt_size; i++ )
+      {
         size->cvt[i] = FT_MulFix( face->cvt[i], size->ttmetrics.scale );
+        FT_TRACE6(( "  %3d: %d (%f)\n",
+                    i, face->cvt[i], size->cvt[i] / 64.0 ));
+      }
+      FT_TRACE6(( "\n" ));
 
       /* all twilight points are originally zero */
       for ( i = 0; i < (FT_UInt)size->twilight.n_points; i++ )
