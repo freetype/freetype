13034e54aac631f5ca8b2dd27a64e5ac5c5f6fbc
Werner Lemberg <wl@gnu.org>
2018-08-26 12:22:51 +0200

    * src/pshint/pshalgo.c (psh_hint_overlap): Fix numeric overflow.
    Reported as
    
      https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=10057

--- src/pshinter/pshalgo.c
+++ src/pshinter/pshalgo.c
@@ -53,8 +53,8 @@
   psh_hint_overlap( PSH_Hint  hint1,
                     PSH_Hint  hint2 )
   {
-    return hint1->org_pos + hint1->org_len >= hint2->org_pos &&
-           hint2->org_pos + hint2->org_len >= hint1->org_pos;
+    return ADD_INT( hint1->org_pos, hint1->org_len ) >= hint2->org_pos &&
+           ADD_INT( hint2->org_pos, hint2->org_len ) >= hint1->org_pos;
   }
 
 
