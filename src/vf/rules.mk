51e77c8217f0a938716ab5edce8d63506cb2dc12
Parth Wazurkar <parthwazurkar@gmail.com>
2018-12-06 00:19:11 +0530

    [vf] Modify makefiles.

--- src/vf/rules.mk
+++ src/vf/rules.mk
@@ -1,5 +1,5 @@
 #
-# FreeType 2 PK driver configuration rules
+# FreeType 2 VF driver configuration rules
 #
 
 
@@ -13,58 +13,58 @@
 # fully.
 
 
-# pk driver directory
+# vf driver directory
 #
-PK_DIR := $(SRC_DIR)/pk
+VF_DIR := $(SRC_DIR)/vf
 
 
-PK_COMPILE := $(CC) $(ANSIFLAGS)                            \
-                     $I$(subst /,$(COMPILER_SEP),$(PK_DIR)) \
+VF_COMPILE := $(CC) $(ANSIFLAGS)                            \
+                     $I$(subst /,$(COMPILER_SEP),$(VF_DIR)) \
                      $(INCLUDE_FLAGS)                        \
                      $(FT_CFLAGS)
 
 
-# pk driver sources (i.e., C files)
+# vf driver sources (i.e., C files)
 #
-PK_DRV_SRC :=  $(PK_DIR)/pklib.c \
-               $(PK_DIR)/pkdrivr.c
+VF_DRV_SRC :=  $(VF_DIR)/vflib.c \
+               $(VF_DIR)/vfdrivr.c
 
 
-# pk driver headers
+# vf driver headers
 #
-PK_DRV_H :=  $(PK_DIR)/pk.h \
-             $(PK_DIR)/pkdrivr.h \
-             $(PK_DIR)/pkerror.h
+VF_DRV_H :=  $(VF_DIR)/vf.h \
+             $(VF_DIR)/vfdrivr.h \
+             $(VF_DIR)/vferror.h
 
-# pk driver object(s)
+# vf driver object(s)
 #
-#   PK_DRV_OBJ_M is used during `multi' builds
-#   PK_DRV_OBJ_S is used during `single' builds
+#   VF_DRV_OBJ_M is used during `multi' builds
+#   VF_DRV_OBJ_S is used during `single' builds
 #
-PK_DRV_OBJ_M := $(PK_DRV_SRC:$(PK_DIR)/%.c=$(OBJ_DIR)/%.$O)
-PK_DRV_OBJ_S := $(OBJ_DIR)/pk.$O
+VF_DRV_OBJ_M := $(VF_DRV_SRC:$(VF_DIR)/%.c=$(OBJ_DIR)/%.$O)
+VF_DRV_OBJ_S := $(OBJ_DIR)/vf.$O
 
-# pk driver source file for single build
+# vf driver source file for single build
 #
-PK_DRV_SRC_S := $(PK_DIR)/pk.c
+VF_DRV_SRC_S := $(VF_DIR)/vf.c
 
 
-# pk driver - single object
+# vf driver - single object
 #
-$(PK_DRV_OBJ_S): $(PK_DRV_SRC_S) $(PK_DRV_SRC) $(FREETYPE_H) $(PK_DRV_H)
-	$(PK_COMPILE) $T$(subst /,$(COMPILER_SEP),$@ $(PK_DRV_SRC_S))
+$(VF_DRV_OBJ_S): $(VF_DRV_SRC_S) $(VF_DRV_SRC) $(FREETYPE_H) $(VF_DRV_H)
+	$(VF_COMPILE) $T$(subst /,$(COMPILER_SEP),$@ $(VF_DRV_SRC_S))
 
 
-# pk driver - multiple objects
+# vf driver - multiple objects
 #
-$(OBJ_DIR)/%.$O: $(PK_DIR)/%.c $(FREETYPE_H) $(PK_DRV_H)
-	$(PK_COMPILE) $T$(subst /,$(COMPILER_SEP),$@ $<)
+$(OBJ_DIR)/%.$O: $(VF_DIR)/%.c $(FREETYPE_H) $(VF_DRV_H)
+	$(VF_COMPILE) $T$(subst /,$(COMPILER_SEP),$@ $<)
 
 
 # update main driver object lists
 #
-DRV_OBJS_S += $(PK_DRV_OBJ_S)
-DRV_OBJS_M += $(PK_DRV_OBJ_M)
+DRV_OBJS_S += $(VF_DRV_OBJ_S)
+DRV_OBJS_M += $(VF_DRV_OBJ_M)
 
 
 # EOF
