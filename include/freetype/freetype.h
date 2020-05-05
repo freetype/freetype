132f19b779828b194b3fede187cee719785db4d8
Werner Lemberg <wl@gnu.org>
2020-05-05 20:14:14 +0200

    * Version 2.10.2 released. ==========================
    Tag sources with `VER-2-10-2'.
    
    * docs/VERSION.TXT: Add entry for version 2.10.2.
    
    * README, Jamfile (RefDoc), src/base/ftver.rc,
    builds/windows/vc2010/index.html, builds/windows/visualc/index.html,
    builds/windows/visualce/index.html,
    builds/wince/vc2005-ce/index.html,
    builds/wince/vc2008-ce/index.html, docs/freetype-config.1:
    s/2.10.1/2.10.2/, s/2101/2102/.
    
    * include/freetype/freetype.h (FREETYPE_PATCH): Set to 2.
    
    * builds/unix/configure.raw (version_info): Set to 23:2:17.
    * CMakeLists.txt (VERSION_PATCH): Set to 2.
    
    * docs/CHANGES: Updated.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -4782,7 +4782,7 @@ FT_BEGIN_HEADER
    */
 #define FREETYPE_MAJOR  2
 #define FREETYPE_MINOR  10
-#define FREETYPE_PATCH  1
+#define FREETYPE_PATCH  2
 
 
   /**************************************************************************
