3f70e6d20c82b28174096adcd0657b3c998b007b
Alexei Podtelezhnikov <apodtele@gmail.com>
2020-05-12 00:38:46 -0400

    [smooth] Turn on LCD filtering during FreeType initialization.
    * src/smooth/ftsmooth.c (ft_smooth_init): Enable LCD filtering.
    
    * include/freetype/ftlcdfil.h: Document it, remove patent warnings.
    * include/freetype/freetype.h (FT_Render_Mode): Updated.
    * include/freetype/config/ftoption.h, devel/ftoption.h
    [FT_CONFIG_OPTION_SUBPIXEL_RENDERING]: Do not mention patents.

--- include/freetype/config/ftoption.h
+++ include/freetype/config/ftoption.h
@@ -121,10 +121,8 @@ FT_BEGIN_HEADER
    * mitigate color fringes inherent to this technology, you also need to
    * explicitly set up LCD filtering.
    *
-   * Note that this feature is covered by several Microsoft patents and
-   * should not be activated in any default build of the library.  When this
-   * macro is not defined, FreeType offers alternative LCD rendering
-   * technology that produces excellent output without LCD filtering.
+   * When this macro is not defined, FreeType offers alternative LCD
+   * rendering technology that produces excellent output.
    */
 /* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */
 
