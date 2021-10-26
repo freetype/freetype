f6a2237ff6af19b8af3fd12ad13068a6e52ab177
Anurag Thakur <anurag105csec21@bpitindia.edu.in>
2021-10-26 13:11:17 +0530

    Populate errors header for 'dense' renderer

--- src/dense/ftdenseerrs.h
+++ src/dense/ftdenseerrs.h
@@ -1,7 +1,16 @@
-/** Common error defines */
 
 #ifndef FTDENSEERRS_H_
 #define FTDENSEERRS_H_
+
+#include <freetype/ftmoderr.h>
+
+#undef FTERRORS_H_
+
+#undef FT_ERR_PREFIX
+#define FT_ERR_PREFIX Dense_Err_
+#define FT_ERR_BASE   FT_Mod_Err_Dense
+
+#include <freetype/fterrors.h>
 
 #endif /* FTDENSEERRS_H_ */
 
