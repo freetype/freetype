34fc55de47a5abe310f3c5a3f7d8cf9c5443e07f
Alexei Podtelezhnikov <apodtele@gmail.com>
2021-12-10 21:55:08 -0500

    [truetype] Relocate subpixel flag setting.
    Eventually, we will use these flags to assess the `hdmx` usability
    prior to the glyph parsing.
    
    * src/truetype/ttinterp.c (TT_RunIns): Move the flag setting to...
    * src/truetype/ttgload.c (tt_loader_init): ...here where it belongs
    and which is executed before each glyph is loaded.

--- src/truetype/ttgload.c
+++ src/truetype/ttgload.c
@@ -2720,7 +2720,34 @@
       if ( driver->interpreter_version == TT_INTERPRETER_VERSION_38 &&
            exec->GS.instruct_control & 4                            )
         exec->ignore_x_mode = 0;
-#endif
+
+      exec->iup_called = FALSE;
+#endif /* TT_SUPPORT_SUBPIXEL_HINTING_INFINALITY */
+
+#ifdef TT_SUPPORT_SUBPIXEL_HINTING_MINIMAL
+      /*
+       * Toggle backward compatibility according to what font wants, except
+       * when
+       *
+       * 1) we have a `tricky' font that heavily relies on the interpreter to
+       *    render glyphs correctly, for example DFKai-SB, or
+       * 2) FT_RENDER_MODE_MONO (i.e, monochome rendering) is requested.
+       *
+       * In those cases, backward compatibility needs to be turned off to get
+       * correct rendering.  The rendering is then completely up to the
+       * font's programming.
+       *
+       */
+      if ( driver->interpreter_version == TT_INTERPRETER_VERSION_38 &&
+           subpixel_hinting_lean                                    &&
+           !FT_IS_TRICKY( glyph->face )                             )
+        exec->backward_compatibility = !( exec->GS.instruct_control & 4 );
+      else
+        exec->backward_compatibility = FALSE;
+
+      exec->iupx_called = FALSE;
+      exec->iupy_called = FALSE;
+#endif /* TT_SUPPORT_SUBPIXEL_HINTING_MINIMAL */
 
       exec->pedantic_hinting = FT_BOOL( load_flags & FT_LOAD_PEDANTIC );
       loader->exec = exec;
