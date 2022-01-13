ee52b57121c2dcb74e0c30d98cdb3bae5b3e7cb9
Alexei Podtelezhnikov <apodtele@gmail.com>
2022-01-13 09:45:48 -0500

    * src/lzw/ftlzw.c (FT_Stream_OpenLZW): Avoid unnecessary zeroing.

--- src/lzw/ftlzw.c
+++ src/lzw/ftlzw.c
@@ -344,7 +344,7 @@
   {
     FT_Error    error;
     FT_Memory   memory;
-    FT_LZWFile  zip = NULL;
+    FT_LZWFile  zip;
 
 
     if ( !stream || !source )
@@ -369,7 +369,7 @@
     FT_ZERO( stream );
     stream->memory = memory;
 
-    if ( !FT_NEW( zip ) )
+    if ( !FT_QNEW( zip ) )
     {
       error = ft_lzw_file_init( zip, stream, source );
       if ( error )
