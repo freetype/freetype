f6810999181e195671a79e72b54768c51a6af784
Anurag Thakur <anurag105csec21@bpitindia.edu.in>
2022-11-19 00:15:20 +0530

    [dense] Populate headers for 'dense' module
    * src/dense/ftdenserend.h (ft_dense_renderer_class): New Structures
    
    * src/dense/ftdense.h (dense_worker): New Structure
    (dense_render_line, dense_render_quadratic, dense_render_cubic): New
    function declarations
    (ft_dense_raster): Export dense raster funcs
    
    * src/dense/ftdenseerrs.h: Add module error setup

--- src/dense/ftdenserend.h
+++ src/dense/ftdenserend.h
@@ -1,6 +1,26 @@
-
+/* Dense renderer interface (specification) */
 #ifndef FTDENSEREND_H_
 #define FTDENSEREND_H_
+
+
+#include <freetype/ftmodapi.h>
+#include <freetype/ftrender.h>
+#include <freetype/internal/ftobjs.h>
+
+FT_BEGIN_HEADER
+
+/**************************************************************************
+ *
+ * @renderer:
+ *   ft_dense_renderer_class
+ *
+ * @description:
+ *   Renderer to convert @FT_Outline to bitmaps.
+ *
+ */
+FT_DECLARE_RENDERER( ft_dense_renderer_class )
+
+FT_END_HEADER
 
 #endif /* FTDENSEREND_H_ */
 
