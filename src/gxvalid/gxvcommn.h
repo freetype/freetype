e0e5b838e508aa4043c0e27d8403ab7be2a46153
Alexei Podtelezhnikov <apodtele@gmail.com>
2023-09-25 22:26:15 -0400

    * src/gxvalid/gxvcommn.h (GXV_USHORT_TO_SHORT): Removed.

--- src/gxvalid/gxvcommn.h
+++ src/gxvalid/gxvcommn.h
@@ -516,9 +516,6 @@ FT_BEGIN_HEADER
               FT_INVALID_TOO_SHORT;                         \
           FT_END_STMNT
 
-#define GXV_USHORT_TO_SHORT( _us )                                    \
-          ( ( 0x8000U < ( _us ) ) ? ( ( _us ) - 0x8000U ) : ( _us ) )
-
 #define GXV_STATETABLE_HEADER_SIZE  ( 2 + 2 + 2 + 2 )
 #define GXV_STATEHEADER_SIZE        GXV_STATETABLE_HEADER_SIZE
 
