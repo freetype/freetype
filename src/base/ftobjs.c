c107e5a76e83b41eb88d2ce0232d9a76ab616a5b
Anurag Thakur <anurag105csec21@bpitindia.edu.in>
2023-09-30 14:24:24 +0530

    prevent segfault

--- src/base/ftobjs.c
+++ src/base/ftobjs.c
@@ -3140,7 +3140,7 @@ int conic_to2(FT_GlyphSlot* slot, FT_Vector *control, FT_Vector *from, FT_Vector
       face->garray = (FT_GlyphSlot*)malloc(
           face->driver->clazz->slot_object_size * face->num_glyphs );
       //error           = FT_Set_Char_Size( face, 0, 160 * 64, 300, 300 );
-      error           = FT_Set_Pixel_Sizes( face, 0, 500);
+      error           = FT_Set_Pixel_Sizes( face, 0, 320);
       // int glyph_index = FT_Get_Char_Index( face, 'A' );
       // error           = FT_Load_Glyph( face, glyph_index, FT_LOAD_NO_HINTING );
 
@@ -3154,7 +3154,7 @@ int conic_to2(FT_GlyphSlot* slot, FT_Vector *control, FT_Vector *from, FT_Vector
         face->garray[gindex]->face = face;
         face->garray[gindex]->glyph_index = gindex;
         ft_glyphslot_init(face->garray[gindex]);
-        face->garray[gindex]->next = face->garray[gindex];
+        face->garray[gindex]->next = NULL;
         *face->glyph = *face->garray[gindex];
 
         FT_Load_Glyph(face, gindex, FT_LOAD_NO_HINTING);
