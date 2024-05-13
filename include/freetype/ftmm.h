68399b4244d85a1acc18ce669176b531b2eb7e32
Alexei Podtelezhnikov <apodtele@gmail.com>
2024-05-13 16:57:35 +0000

    * include/freetype/ftmm.h: Include `freetype.h`.
    Reported by Ben Wagner, see !326.

--- include/freetype/ftmm.h
+++ include/freetype/ftmm.h
@@ -19,6 +19,14 @@
 #ifndef FTMM_H_
 #define FTMM_H_
 
+#include <freetype/freetype.h>
+
+#ifdef FREETYPE_H
+#error "freetype.h of FreeType 1 has been loaded!"
+#error "Please fix the directory search order for header files"
+#error "so that freetype.h of FreeType 2 is found first."
+#endif
+
 
 FT_BEGIN_HEADER
 
