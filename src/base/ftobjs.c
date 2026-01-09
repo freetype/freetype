341049a95bacfc5debf6c9daf537b7acec27f3dd
Alexei Podtelezhnikov <apodtele@gmail.com>
2026-01-09 18:22:15 -0500

    [base] Set ppem-based rendering limits.
    Many rendering timeouts occur when the outline stretches far beyond
    the Em size.  Limiting the bounding box to 10 Em should reject such
    requests.  It is still to possible scribble excessively within the set
    limits.  At least, the memory usage will be curtained.
    
    * src/base/ftobjs.c (ft_glyphslot_free_bitmap): Revise limits based
    on ppem when available.

--- src/base/ftobjs.c
+++ src/base/ftobjs.c
@@ -524,12 +524,22 @@
     bitmap->rows       = (unsigned int)height;
     bitmap->pitch      = pitch;
 
-    if ( pbox.xMin < -0x8000 || pbox.xMax > 0x7FFF ||
-         pbox.yMin < -0x8000 || pbox.yMax > 0x7FFF )
+    /* Flag the bounding box size unsuitable for rendering. */
+    /* FT_Renderer modules should check the return value.   */
+    /* The limit is based on the ppem value when available. */ 
     {
-      FT_TRACE3(( "ft_glyphslot_preset_bitmap: [%ld %ld %ld %ld]\n",
-                  pbox.xMin, pbox.yMin, pbox.xMax, pbox.yMax ));
-      return 1;
+      FT_Face  face = slot->face;
+      FT_Pos   xlim = face ? 10 * face->size->metrics.x_ppem : 0x8000;
+      FT_Pos   ylim = face ? 10 * face->size->metrics.y_ppem : 0x8000;
+
+
+      if ( pbox.xMin < -xlim || pbox.xMax > xlim ||
+           pbox.yMin < -ylim || pbox.yMax > ylim )
+      {
+        FT_TRACE3(( "ft_glyphslot_preset_bitmap: [%ld %ld %ld %ld]\n",
+                    pbox.xMin, pbox.yMin, pbox.xMax, pbox.yMax ));
+        return 1;
+      }
     }
 
     return 0;
