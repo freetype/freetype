freetype/freetype

commit bda1d87f694dd228efd7fc93481afd3a9b415a03

* src/sdf/ftsdf.c (sdf_generate_bounding_box): Fix allocation.
Reported in

  https://mail.gnu.org/archive/html/freetype-devel/2026-02/msg00005.html

src/sdf/ftsdf.c
@@ -3281,7 +3281,7 @@
       goto Exit;
     }
 
-    if ( FT_NEW_ARRAY( dists, bitmap->rows * bitmap->width ) )
+    if ( FT_ALLOC_MULT( dists, bitmap->rows, bitmap->width ) )
       goto Exit;
 
     contours = shape->contours;

commit 4130e62d23481aae7e61ded08ef3837859b62f69

* src/sdf/ftsdf.c (sdf_generate_bounding_box): Fix thinko.
Bug introduced in commit bda1d87f694dd.

We would need an `FT_NEW_ARRAY_MULT` array...

Reported as

  https://issues.oss-fuzz.com/issues/494362618

src/sdf/ftsdf.c
@@ -3281,7 +3281,12 @@
       goto Exit;
     }
 
-    if ( FT_ALLOC_MULT( dists, bitmap->rows, bitmap->width ) )
+    if ( bitmap->rows > FT_INT_MAX / bitmap->width )
+    {
+      error = FT_THROW( Array_Too_Large );
+      goto Exit;
+    }
+    if ( FT_NEW_ARRAY( dists, bitmap->rows * bitmap->width ) )
       goto Exit;
 
     contours = shape->contours;