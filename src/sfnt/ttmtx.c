freetype/freetype

commit 5987a9f51d7002f7ca375a34c2b857841d82a91c

[ttmtx] Fix check for being non-default variation position.
If user explicitly sets variations to `NULL` after it being set to something
else, `face->blend` will still be non-`NULL`.  The intention here however is
correctly captured by `FT_IS_VARIATION`.

* src/sfnt/ttmtx.c (tt_face_get_metrics): Use `FT_IS_VARIATION`.

src/sfnt/ttmtx.c
@@ -306,7 +306,7 @@
     }
 
 #ifdef TT_CONFIG_OPTION_GX_VAR_SUPPORT
-    if ( var && face->blend )
+    if ( var && FT_IS_VARIATION( &face->root ) )
     {
       FT_Face  f = FT_FACE( face );
       FT_Int   a = (FT_Int)*aadvance;

commit 5178bdc2af099c38d1ca6588904ed7d9ba3f849f

[sfnt] Correctly handle HVAR/VVAR advance values for named instances.
This bug was introduced with commit 5987a9f51d.

Fixes issue #1452, which comes with a detailed analysis and a patch provided
by an LLM.

* src/sfnt/ttmtx.c (tt_face_get_metrics) [TT_CONFIG_OPTION_GX_VAR_SUPPORT]:
  Also check `FT_IS_NAMED_INSTANCE`.

src/sfnt/ttmtx.c
@@ -306,7 +306,9 @@
     }
 
 #ifdef TT_CONFIG_OPTION_GX_VAR_SUPPORT
-    if ( var && FT_IS_VARIATION( &face->root ) )
+    if ( var                                     &&
+         ( FT_IS_VARIATION( &face->root )      ||
+           FT_IS_NAMED_INSTANCE( &face->root ) ) )
     {
       FT_Face  f = FT_FACE( face );
       FT_Int   a = (FT_Int)*aadvance;